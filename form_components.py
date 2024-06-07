02f82a5fb72794ed245b7eea110bcbe4ca155475
alx-zhu <alexanderzhu07@gmail.com>
2024-06-06 21:15:12 -0400

    Connected linkedin parsing to streamlit frontend and added a modal

--- form_components.py
+++ form_components.py
@@ -1,12 +1,21 @@
 import streamlit as st
 from datetime import date
 from constants import SAMPLE_RESUME
+from io import BytesIO
+from linkedin_pdf_parser import parse_linkedin_profile_pdf
 
 
 def form_submit_button(id=""):
     if st.button("Submit", key=f"submit_button_{id}"):
         st.session_state.is_chat_open = True
         st.rerun()
+
+
+def set_form_fields_from_profile(profile):
+    st.session_state.intro = profile["intro"]
+    st.session_state.experience = profile["experience"]
+    st.session_state.projects = profile["projects"]
+    st.session_state.education = profile["education"]
 
 
 def reset_all_form_fields():
@@ -20,9 +29,29 @@ def reset_all_form_fields():
     st.session_state.education = []
 
 
+@st.experimental_dialog("Upload Your LinkedIn Profile")
+def upload_linkedin_profile():
+    uploaded_file = st.file_uploader("Choose a PDF file", type=["pdf"])
+    if uploaded_file is not None:
+        # Read the uploaded file as bytes
+        pdf_bytes = uploaded_file.getvalue()
+        if st.button("Submit"):
+            parsed_profile = parse_linkedin_profile_pdf(BytesIO(pdf_bytes))
+            set_form_fields_from_profile(parsed_profile)
+            st.rerun()
+    st.divider()
+    with st.expander("How to export your Linkedin profile"):
+        st.markdown("### Exporting Your Profile from LinkedIn")
+        st.markdown(
+            '1. Navigate to your LinkedIn Profile Page \n2. Click on the "More" Button. \n3. Click "Save to PDF"'
+        )
+        st.image("import_instructions.png")
+
+
 def render_form():
     if "intro" not in st.session_state:
         reset_all_form_fields()
+
     st.title("Create Your Profile")
     introduction_form()
     st.divider()
@@ -35,13 +64,10 @@ def render_form():
 
     with st.sidebar:
         if st.button("Import LinkedIn Profile", key="import_from_linkedin"):
-            pass
+            upload_linkedin_profile()
 
         if st.button("Use Alex's Sample Resume", key="use_sample"):
-            st.session_state.intro = SAMPLE_RESUME["intro"]
-            st.session_state.experience = SAMPLE_RESUME["experience"]
-            st.session_state.projects = SAMPLE_RESUME["projects"]
-            st.session_state.education = SAMPLE_RESUME["education"]
+            set_form_fields_from_profile(SAMPLE_RESUME)
             st.rerun()
 
         st.divider()
@@ -65,13 +91,15 @@ def introduction_form():
 
     st.subheader("Introduction")
     st.session_state.intro["name"] = st.text_input(
-        f"Name", value=st.session_state.intro["name"], key="name"
+        f"Name", value=st.session_state.intro.get("name", ""), key="name"
     )
     st.session_state.intro["email"] = st.text_input(
-        f"Email", value=st.session_state.intro["email"], key="email"
+        f"Email", value=st.session_state.intro.get("email", ""), key="email"
     )
     st.session_state.intro["summary"] = st.text_area(
-        f"Summary", value=st.session_state.intro["summary"].strip(), key="summary"
+        f"Summary",
+        value=st.session_state.intro.get("summary", "").strip(),
+        key="summary",
     )
 
 
@@ -91,47 +119,49 @@ def experience_form():
                 "description": "",
             }
         )
-        st.rerun()
 
     # Display experience forms
     st.subheader("Experience")
 
-    for i, experience in enumerate(st.session_state.experience):
-        title = (
-            f"{experience['title']} @ {experience['company']}"
-            if experience["title"]
-            else "Untitled Experience"
-        )
-        with st.expander(title, expanded=True):
-            st.session_state.experience[i]["title"] = st.text_input(
-                f"Job Title",
-                value=experience["title"],
-                key=f"job_title_{i}",
-            )
-            st.session_state.experience[i]["company"] = st.text_input(
-                f"Company", value=experience["company"], key=f"company_{i}"
-            )
-            st.session_state.experience[i]["start"] = st.date_input(
-                f"Start Date",
-                value=experience["start"],
-                key=f"start_date_{i}",
-            )
-            st.session_state.experience[i]["end"] = st.date_input(
-                f"End Date", value=experience["end"], key=f"end_date_{i}"
-            )
-            st.session_state.experience[i]["description"] = st.text_area(
-                f"Description",
-                value=experience["description"].strip(),
-                key=f"description_{i}",
-            )
-
-            if st.button("Delete", key=f"delete_button_{i}"):
-                del st.session_state.experience[i]
-                st.rerun()
+    forms_container = st.container()
 
     # Add experience button
     if st.button("Add Experience"):
         add_experience_form()
+
+    with forms_container:
+        for i, experience in enumerate(st.session_state.experience):
+            title = (
+                f"{experience['title']} @ {experience['company']}"
+                if experience["title"]
+                else "Untitled Experience"
+            )
+            with st.expander(title, expanded=True):
+                st.session_state.experience[i]["title"] = st.text_input(
+                    f"Job Title",
+                    value=experience.get("title", ""),
+                    key=f"job_title_{i}",
+                )
+                st.session_state.experience[i]["company"] = st.text_input(
+                    f"Company", value=experience.get("company", ""), key=f"company_{i}"
+                )
+                st.session_state.experience[i]["start"] = st.date_input(
+                    f"Start Date",
+                    value=experience.get("start", None),
+                    key=f"start_date_{i}",
+                )
+                st.session_state.experience[i]["end"] = st.date_input(
+                    f"End Date", value=experience.get("end", None), key=f"end_date_{i}"
+                )
+                st.session_state.experience[i]["description"] = st.text_area(
+                    f"Description",
+                    value=experience.get("description", "").strip(),
+                    key=f"description_{i}",
+                )
+
+                if st.button("Delete", key=f"delete_button_{i}"):
+                    del st.session_state.experience[i]
+                    st.rerun()
 
 
 def projects_form():
@@ -150,46 +180,52 @@ def projects_form():
                 "description": "",
             }
         )
-        st.rerun()
 
     # Display project forms
     st.subheader("Projects")
 
-    for i, project in enumerate(st.session_state.projects):
-        title = project["title"] if project["title"] else "Untitled Project"
-        with st.expander(
-            title,
-            expanded=True,
-        ):
-            st.session_state.projects[i]["title"] = st.text_input(
-                "Project Title",
-                value=project["title"],
-                key=f"project_title_{i}",
-            )
-            st.session_state.projects[i]["organization"] = st.text_input(
-                "Organization", value=project["organization"], key=f"organization_{i}"
-            )
-            st.session_state.projects[i]["start"] = st.date_input(
-                "Start Date",
-                value=project["start"],
-                key=f"project_start_date_{i}",
-            )
-            st.session_state.projects[i]["end"] = st.date_input(
-                "End Date", value=project["end"], key=f"project_end_date_{i}"
-            )
-            st.session_state.projects[i]["description"] = st.text_area(
-                "Description",
-                value=project["description"].strip(),
-                key=f"project_description_{i}",
-            )
-
-            if st.button("Delete", key=f"project_delete_button_{i}"):
-                del st.session_state.projects[i]
-                st.rerun()
+    forms_container = st.container()
 
     # Add project button
     if st.button("Add Project"):
         add_project_form()
+
+    with forms_container:
+        for i, project in enumerate(st.session_state.projects):
+            title = project["title"] if project["title"] else "Untitled Project"
+            with st.expander(
+                title,
+                expanded=True,
+            ):
+                st.session_state.projects[i]["title"] = st.text_input(
+                    "Project Title",
+                    value=project.get("title", ""),
+                    key=f"project_title_{i}",
+                )
+                st.session_state.projects[i]["organization"] = st.text_input(
+                    "Organization",
+                    value=project.get("organization", ""),
+                    key=f"organization_{i}",
+                )
+                st.session_state.projects[i]["start"] = st.date_input(
+                    "Start Date",
+                    value=project.get("start", None),
+                    key=f"project_start_date_{i}",
+                )
+                st.session_state.projects[i]["end"] = st.date_input(
+                    "End Date",
+                    value=project.get("end", None),
+                    key=f"project_end_date_{i}",
+                )
+                st.session_state.projects[i]["description"] = st.text_area(
+                    "Description",
+                    value=project.get("description", "").strip(),
+                    key=f"project_description_{i}",
+                )
+
+                if st.button("Delete", key=f"project_delete_button_{i}"):
+                    del st.session_state.projects[i]
+                    st.rerun()
 
 
 def education_form():
@@ -212,46 +248,50 @@ def education_form():
     # Display education forms
     st.subheader("Education")
 
-    for i, ed in enumerate(st.session_state.education):
-        title = f"{ed['degree']} @ {ed['school']}"
-        with st.expander(title, expanded=True):
-            st.session_state.education[i]["school"] = st.text_input(
-                "School/University",
-                value=ed["school"],
-                key=f"school_{i}",
-            )
-            st.session_state.education[i]["degree"] = st.text_input(
-                "Degree", value=ed["degree"], key=f"degree_{i}"
-            )
-            st.session_state.education[i]["start"] = st.date_input(
-                "Start Date",
-                value=ed["start"],
-                key=f"ed_start_date_{i}",
-            )
-            st.session_state.education[i]["end"] = st.date_input(
-                "End Date", value=ed["end"], key=f"ed_end_date_{i}"
-            )
-            st.session_state.education[i]["gpa"] = st.number_input(
-                "GPA",
-                value=float(ed["gpa"]),
-                key=f"ed_gpa_{i}",
-                min_value=0.0,
-                max_value=5.0,
-                step=0.01,
-            )
-            st.session_state.education[i]["description"] = st.text_area(
-                "Description",
-                value=ed["description"].strip(),
-                key=f"ed_description_{i}",
-            )
+    forms_container = st.container()
 
-            if st.button("Delete", key=f"ed_delete_button_{i}"):
-                del st.session_state.education[i]
-                st.rerun()
-
+    # Add project button
     # Add education button
     if st.button("Add Education"):
         add_education_form()
+
+    with forms_container:
+        for i, ed in enumerate(st.session_state.education):
+            title = f"{ed.get('degree', '')} @ {ed.get('school', '')}"
+            with st.expander(title, expanded=True):
+                st.session_state.education[i]["school"] = st.text_input(
+                    "School/University",
+                    value=ed.get("school", ""),
+                    key=f"school_{i}",
+                )
+                st.session_state.education[i]["degree"] = st.text_input(
+                    "Degree", value=ed.get("degree", ""), key=f"degree_{i}"
+                )
+                st.session_state.education[i]["start"] = st.date_input(
+                    "Start Date",
+                    value=ed.get("start", None),
+                    key=f"ed_start_date_{i}",
+                )
+                st.session_state.education[i]["end"] = st.date_input(
+                    "End Date", value=ed.get("end", None), key=f"ed_end_date_{i}"
+                )
+                st.session_state.education[i]["gpa"] = st.number_input(
+                    "GPA",
+                    value=float(ed.get("gpa", 0.0)),
+                    key=f"ed_gpa_{i}",
+                    min_value=0.0,
+                    max_value=5.0,
+                    step=0.01,
+                )
+                st.session_state.education[i]["description"] = st.text_area(
+                    "Description",
+                    value=ed.get("description", "").strip(),
+                    key=f"ed_description_{i}",
+                )
+
+                if st.button("Delete", key=f"ed_delete_button_{i}"):
+                    del st.session_state.education[i]
+                    st.rerun()
 
 
 # {
--- linkedin_pdf_parser.py
+++ linkedin_pdf_parser.py
@@ -1,6 +1,8 @@
 import re
 from datetime import datetime, date
 import pdfplumber
+from io import BytesIO
+from typing import Union
 
 # text sizes:
 # headers 15.75
@@ -16,15 +18,28 @@ JOB_TITLE_THRESHOLD = 11.0
 DATE_LOCATION_DESCRIPTION_THRESHOLD = 10.0
 DATE_RANGE_REGEX = r"\b([A-Za-z]+ \d{4}) - (([A-Za-z]+ \d{4})|Present)\b"
 DATE_FORMAT = "%B %Y"
+EMAIL_REGEX = r"\b[a-zA-Z0-9._-]+@[a-zA-Z0-9]+(\.[a-zA-Z]{2,}){1,}\b"
 
 
-def extract_sidebar(file_path):
-    pass
+def extract_email(pdf_file: Union[str, BytesIO]):
+    with pdfplumber.open(pdf_file) as pdf:
+        if len(pdf.pages) > 0:
+            page = pdf.pages[0]
+            # Crop to the sidebar
+            cropped = page.crop((0, 0, page.width / 3, page.height))
+            text = cropped.extract_text_simple()
+            match = re.search(EMAIL_REGEX, text)
+            # Extract and print the first email address found
+            if match:
+                email = match.group(0)
+                return email
+            else:
+                return ""
 
 
-def extract_sections(file_path):
+def extract_sections(pdf_file: Union[str, BytesIO]):
     page_regex = re.compile(r"Page\s+\d+\s+of\s+\d+", re.IGNORECASE)
-    with pdfplumber.open(file_path) as pdf:
+    with pdfplumber.open(pdf_file) as pdf:
         sections = {}
         section_text = []
         # First section is name and headline
@@ -110,8 +125,10 @@ def extract_education(education_lines):
         if size > COMPANY_UNIVERSITY_THRESHOLD:
             if current:
                 current["description"] = description
+                current["degree"] = current.get("degree", "")
                 current["start"] = current.get("start", None)
                 current["end"] = current.get("end", None)
+                current["gpa"] = 0.0
                 education.append(current)
                 description = ""
             current = {"school": line}
@@ -139,19 +156,23 @@ def extract_education(education_lines):
 
             else:
                 description += " " + line
+
+    # Barring against education entries that are missing fields
     current["description"] = description
+    current["degree"] = current.get("degree", "")
     current["start"] = current.get("start", None)
     current["end"] = current.get("end", None)
+    current["gpa"] = 0.0
     education.append(current)
     return education
 
 
-def parse_linkedin_profile_pdf(file_path):
-    name, sections = extract_sections(file_path)
+def parse_linkedin_profile_pdf(pdf_file: Union[str, BytesIO]):
+    name, sections = extract_sections(pdf_file)
     return {
         "intro": {
             "name": name,
-            "email": "",
+            "email": extract_email(pdf_file),
             "summary": extract_summary(sections["summary"]),
         },
         "experience": extract_experience(sections["experience"]),
