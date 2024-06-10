26cc1e9fcae7d9ca96702b4b445895868842de86
alx-zhu <alexanderzhu07@gmail.com>
2024-06-09 22:03:18 -0400

    Adding template code for emailing

---
+++ email_sender.py
@@ -0,0 +1,17 @@
+import smtplib
+
+
+# Template code from https://www.geeksforgeeks.org/send-mail-gmail-account-using-python/
+def send_email():
+    # creates SMTP session
+    s = smtplib.SMTP("smtp.gmail.com", 587)
+    # start TLS for security
+    s.starttls()
+    # Authentication
+    s.login("sender_email_id", "sender_email_id_password")
+    # message to be sent
+    message = "Message_you_need_to_send"
+    # sending the mail
+    s.sendmail("sender_email_id", "receiver_email_id", message)
+    # terminating the session
+    s.quit()
